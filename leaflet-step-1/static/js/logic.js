2709aae74b22136164a847c76ed8b6d70a65e0ef
Paola Lopez <paola.lopez1@yahoo.com>
2021-02-17 16:07:35 -0800

    in the process of fixing any errors, map isn't showing up

--- leaflet-step-1/static/js/logic.js
+++ leaflet-step-1/static/js/logic.js
@@ -1,5 +1,5 @@
 // Creating map object
-var myMap = L.map("map", {
+var myMap = L.map("mapid", {
     center: [40.7, -73.95],
     zoom: 5
   });
@@ -18,20 +18,22 @@ var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}
 url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
 var geoJson
 
-// Creating function 
-d3.json(url, function(data) {
-    function collectionInfo(features) {
+// Create the createMap function
+d3.json(url, data=> {
+    console.log(data);
+    var features = [];
+    data.features.forEach(element=> {
         return {
             fillOpacity: 0.75,
             color: "black",
-            fillColor: getColor(features.geometry.coordinates[2]),
-            radius: getRadius(features.properties.mag),
+            fillColor: getColor(element.geometry.coordinates[2]),
+            radius: getRadius(element.properties.mag),
             stroke: true,
             weight: 0.25
         };
-    }
+    })
 
-    // setting color from depth
+     // setting color from depth
     function getColor(depth) {
         switch (true) {
             case depth > 90:
@@ -49,7 +51,7 @@ d3.json(url, function(data) {
         }
     }
 
-    // setting radius from magnitude
+//     // setting radius from magnitude
     function getRadius(magnitude) {
         if (magnitude === 0) {
             return 1;
@@ -57,29 +59,28 @@ d3.json(url, function(data) {
         return magnitude * 4;
     }
 
-    // creating GEOjson layer
+//     // creating GEOjson layer
     L.geoJson(data, {
         //create circle marker
-        pointToLayer: function(feature, coordinates) {
+        pointToLayer: function(features, coordinates) {
             return L.circleMarker(coordinates);
         },
 
-        style: collectionInfo,
-
         // popup for each marker
+        onEachFeature: function(features, layer) {
+            layer.bindPopup("Place:" + features.properties.place + "<br>Location:<br>" + features.geometry.coordinates + "<br>Magnitude:<br>" + features.properties.mag)
+        }
+    }).addTo(myMap);
+})
 
-    })
+// LEGEND
 
-}
-    
-    
-    
-    
-    
-    
-    
-    
-//     data=> {
+
+
+
+
+
+// d3.json(url, data=> {
 //     console.log(data);
 //     var features = [];
 //     data.features.forEach(element=> {
@@ -90,4 +91,64 @@ d3.json(url, function(data) {
 //         });
 //     });
 // console.log(features);
+// }).addTo(myMap);
+
+
+
+
+
+// Creating function 
+// d3.json(url, function(data) {
+//     function collectionInfo(features) {
+//         return {
+//             fillOpacity: 0.75,
+//             color: "black",
+//             fillColor: getColor(features.geometry.coordinates[2]),
+//             radius: getRadius(features.properties.mag),
+//             stroke: true,
+//             weight: 0.25
+//         };
+//     }
+
+//     // setting color from depth
+//     function getColor(depth) {
+//         switch (true) {
+//             case depth > 90:
+//                 return "#ff0505";
+//             case depth > 70:
+//                 return "#ff3305";
+//             case depth > 50:
+//                 return "#ff8205";
+//             case depth > 30:
+//                 return "#ffda05";
+//             case depth > 10:
+//                 return "#bcff05";
+//             default:
+//                 return "#82ff05";
+//         }
+//     }
+
+//     // setting radius from magnitude
+//     function getRadius(magnitude) {
+//         if (magnitude === 0) {
+//             return 1;
+//         }
+//         return magnitude * 4;
+//     }
+
+//     // creating GEOjson layer
+//     L.geoJson(data, {
+//         //create circle marker
+//         pointToLayer: function(features, coordinates) {
+//             return L.circleMarker(coordinates);
+//         },
+
+//         style: collectionInfo,
+
+//         // popup for each marker
+//         onEachFeature: function(features, layer) {
+//             layer.bindPopup("Place:" + features.properties.place + <br> "Location:" + features.geometry.coordinates </br> + <br> "Magnitude:" + features.properties.mag </br>)
+//         }
+//     }).addTo(myMap);
+
 // });
